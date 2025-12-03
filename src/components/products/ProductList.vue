9dfa931a0d3ad19dc77e77b97ee7e9a4456e8c79
sypnoze <onurkucukkaya.webdev@gmail.com>
2025-12-03 18:05:22 +0100

    Mise en place API product

--- src/components/products/ProductList.vue
+++ src/components/products/ProductList.vue
@@ -1,12 +1,27 @@
 <script setup>
 import Product from "./Product.vue";
+import {onMounted } from "vue";
+import {productsStore} from "@/stores/products";
+
+const props = defineProps({
+  apiURL: { type: String, required: true },
+});
+
+onMounted(async () => {
+  productsStore.init(props.apiURL);
+});
 </script>
+
 <template>
   <section class="w-full md:w-2/3 px-4 mb-8">
     <!-- Products Section -->
     <h1 class="text-3xl font-bold mb-4">Nouveaux produits</h1>
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-      <product />
+  <Product 
+  v-for="product in productsStore.products"
+  :key="product.id"
+  :product="product"
+  />
     </div>
   </section>
 </template>
---
+++ src/services/DB.js
@@ -0,0 +1,10 @@
+export default class DB {
+
+  static setApiURL(data) {
+    this.apiURL = data;
+  }
+  static async findAll() {
+    const response = await fetch(this.apiURL + "products");
+    return response.json();
+  }
+  }
