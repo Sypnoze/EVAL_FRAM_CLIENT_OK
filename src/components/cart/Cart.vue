ff3a4d5113388d529ae4d1fa3a6b2ee935212df0
sypnoze <onurkucukkaya.webdev@gmail.com>
2025-12-03 16:08:41 +0100

    Découpe Template

---
+++ src/components/cart/Cart.vue
@@ -0,0 +1,20 @@
+<script setup>
+import CartItem from './CartItem.vue';
+import CartTotal from './CartTotal.vue';
+
+</script>
+<template>
+<aside class="w-full md:w-1/3 px-4">
+        <h2 class="text-xl font-bold mb-4">Votre Panier</h2>
+        <div class="cart bg-white rounded-lg shadow animate__animated">
+          <ul class="divide-y divide-gray-200 space-y-4 p-6">
+            
+            <!-- Repeat for other cart items -->
+
+            <CartItem />
+          </ul>
+          <CartTotal/>
+        </div>
+      </aside>
+      </template>
+      <style scoped></style>
